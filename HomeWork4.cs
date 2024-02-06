9bd29222ebd5afa6a5533c398f17be32eeedfc16
AdrianVasquez22 <156941664+AdrianVasquez22@users.noreply.github.com>
2024-02-06 13:51:25 -0600

    Uploading Homework 4

---
+++ HomeWork4.cs
@@ -0,0 +1,26 @@
+﻿namespace HomeWork4;
+
+class Program
+{
+    static void Input(){
+        
+        Console.Write("A = ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("B = ");
+        int b = int.Parse(Console.ReadLine());
+ 
+        if (a > b)
+        {
+            Console.WriteLine("The largest number is: " + a);
+        }
+        else {
+            Console.WriteLine("The largest number is: " + b);
+        }
+    }
+ 
+    public static void Main() 
+    { 
+        Input();
+    }
+    
+}
